995323a23d09925a726d24b2096f24036de5010b
Smita1706 <smitachinchkar1998@gmail.com>
2022-02-22 09:21:35 +0530

    Added : The Bank account program files is added

---
+++ SavingsAccount.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.bankAccount;
+
+public class SavingsAccount {
+	private static double annualInterestRate = 0f;
+	private double savingsBalance;
+	
+	public SavingsAccount( double balance) {
+		this.savingsBalance = balance;
+	}
+	public static double getAnnualInterestRate() {
+		return annualInterestRate;
+	}
+
+	public static void setAnnualInterestRate(double annualInterestRate) {
+		SavingsAccount.annualInterestRate = annualInterestRate;
+	}
+	public double getSavingsBalance() {
+		return savingsBalance;
+	}
+
+	public void setSavingsBalance(double savingsBalance) {
+		this.savingsBalance = savingsBalance;
+	}
+	public double calculateMonthlyInterest() {
+		double monthlyInterest = savingsBalance *(annualInterestRate/100) / 12;
+		savingsBalance += monthlyInterest;
+		return savingsBalance;
+	}
+	public void display(double interest) {
+		System.out.println(" The monthly interest rate is : " + interest);
+		System.out.println(" The new saving balance is : " + savingsBalance);
+	}
+	public static void modifyInterestRate(double newInterestRate) {
+		annualInterestRate = newInterestRate;
+	}
+	
+}
